928b984946797e7a97f0238c225baa62d7fbcf85
AlexNg <contact@ngjx.org>
2024-08-11 21:56:57 +0800

    feat: Add parsing arguments as a CLI
    ---
    
    Signed-off-by: AlexNg <contact@ngjx.org>
    Co-authored-by: ArkStructCodes <153435292+ArkStructCodes@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,54 @@
+use clap::Parser;
+use std::path::PathBuf;
+use std::{thread, time};
+use std::iter::repeat;
+use std::borrow::Cow;
+
 mod string;
 mod styles;
+
+/// Rusty Clock
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The color to display in
+    #[arg(short, long, default_value_t = 218)]
+    color: u8,
+
+    // The spacing between each digits
+    #[arg(long, default_value_t = 2)]
+    spacing: u8,
+
+    /// The style of clock - Either a preset name or a path to a file
+    #[arg(short, long, default_value_t = String::from("curvy"))]
+    style: String,
+
+    /// Whether to run it in debug mode
+    #[arg(short, long, default_value_t = false)]
+    debug: bool,
+
+    /// List styles
+    #[arg(short, long, default_value_t = false)]
+    list_styles: bool,
+}
+
+impl Args {
+    fn get_ascii(&self) -> u8 {
+        self.color
+    }
+}
+
+/// Entrypoint
+/// Parsing arguments
+fn main() {
+    let mut args = Args::parse();
+    args.style = args.style.to_lowercase();
+
+    // stdOut if debug
+    if args.debug {
+        println!("Parsed color: {}", args.color);
+        println!("Parsed spacing: {}", args.spacing);
+        println!("Parsed style: {}", args.style);
+        println!("Parsed list_styles: {}", args.list_styles);
+    }
+}
